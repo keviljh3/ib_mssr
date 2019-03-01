68d8a6770cc9acd6ac9bf3b513c7e2592f4d3092
keviljh3 <kevinljh3@hotmail.com>
2019-03-01 15:54:02 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 killall python                                                                                                  
 killall java
-nohup python /opt/ssr/shadowsocksr-akkariiin-dev/shadowsocks/server.py -p 17520 -k Ssr123456 -m chacha20-ietf -O auth_chain_a -o tls1.2_ticket_auth_compatible -G 32 -g www.yahoo.com > ssr.log 2>&1 &
+nohup python /opt/ssr/shadowsocksr-akkariiin-dev/shadowsocks/server.py -p 8339 -k Ssr123456 -m aes-256-cfb -O auth_aes128_sha1 -o tls1.2_ticket_auth_compatible -G 32 -g www.bing.com > ssr.log 2>&1 &
